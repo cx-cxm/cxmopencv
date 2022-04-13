690e0fea7e9c6a0c3e0fb880054934ee339f29bd
teppeishimakawa <teppei.shimakawa@fujitv.co.jp>
2022-04-13 09:01:40 +0900

    初期登録

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './' // 本番環境にupした際、directry構成が崩れないようにするため超重要!!
 })
